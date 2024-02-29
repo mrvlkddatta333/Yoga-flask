2165ccfac329cf7b17a56781a2ba8b7aa2cbd0ec
mrvlkddatta333 <kesavadattakd333@gmail.com>
2024-02-29 22:19:09 +0530

    first commit

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask,render_template,redirect,url_for,request,flash,session
 from flask_session import Session
 app=Flask(__name__)  #it shows the current module path
 import mysql.connector
+import mysql.connector.pooling
 import os
-mydb=mysql.connector.connect(host='localhost',user='root',password='admin',db='yoga')
+
+conn=mysql.connector.pooling.MySQLConnectionPool(host=host,user=user,password=password,db=db,pool_name='DED',pool_size=25, pool_reset_session=True)
+
+
 app.secret_key='guruji'
 app.config['SESSION_TYPE']='filesystem'
 Session(app)
@@ -28,7 +32,8 @@ def register():
         img.save(os.path.join(static_path,filename))
 
         try:
-            cursor=mydb.cursor(buffered=True)
+            mydb=conn.get_connection()
+            cursor = mydb.cursor(buffered=True)
             cursor.execute('select count(*) from register where first_name=%s',[fname])
             count=cursor.fetchone()[0]
             print(count)
@@ -37,11 +42,21 @@ def register():
         except Exception as e:
             flash('user already existed')
             return redirect(url_for('home'))
+        finally:
+            if mydb.is_connected():
+                mydb.close()
         else:
-            cursor=mydb.cursor(buffered=True)
-            cursor.execute('insert into register(first_name,last_name,phone,email,password,img) values(%s,%s,%s,%s,%s,%s)',[fname,lname,phone,email,password,filename])
-            mydb.commit()
-            cursor.close()
+            try:
+                mydb=conn.get_connection()
+                cursor = mydb.cursor(buffered=True)
+                cursor.execute('insert into register(first_name,last_name,phone,email,password,img) values(%s,%s,%s,%s,%s,%s)',[fname,lname,phone,email,password,filename])
+                mydb.commit()
+                cursor.close()
+            except Exception as e:
+                print(e)
+            finally:
+                if mydb.is_connected():
+                    mydb.close()
             flash('Your Details has registered successfully')
             return redirect(url_for('home'))
     return redirect(url_for('home'))
@@ -52,15 +67,20 @@ def login():
         password=request.form['password']
         print(username)
         try:
-            cursor=mydb.cursor(buffered=True)
+            mydb=conn.get_connection()
+            cursor = mydb.cursor(buffered=True)
             cursor.execute('select count(*) from register where email=%s and password=%s',[username,password])
             count=cursor.fetchone()[0]
+            cursor.close()
             print(count)
             if count==0:
                 raise Exception
         except Exception as e:
             flash('username or password was incorrect')
             return redirect(url_for('login'))
+        finally:
+            if mydb.is_connected():
+                mydb.close()
         else:
             session['user']=username
             if not session.get(username):
@@ -78,15 +98,31 @@ def about():
     return render_template('about.html')
 @app.route('/gurus')
 def gurus():
-    cursor=mydb.cursor(buffered=True)
-    cursor.execute('select * from register')
-    users=cursor.fetchall()
+    try:
+        mydb=conn.get_connection()
+        cursor = mydb.cursor(buffered=True)
+        cursor.execute('select * from register')
+        users=cursor.fetchall()
+        cursor.close()
+    except Exception as e:
+        print(e)
+    finally:
+            if mydb.is_connected():
+                mydb.close()
     return render_template('gurus.html',users=users)
 @app.route('/guru/<user>')
 def guru(user):
-    cursor=mydb.cursor(buffered=True)
-    cursor.execute('select first_name,last_name,phone,img,description from register where email=%s',[user])
-    data=cursor.fetchone()
+    try:
+        mydb=conn.get_connection()
+        cursor = mydb.cursor(buffered=True)
+        cursor.execute('select first_name,last_name,phone,img,description from register where email=%s',[user])
+        data=cursor.fetchone()
+        cursor.close()
+    except Exception as e:
+        print(e)
+    finally:
+            if mydb.is_connected():
+                mydb.close()
     return render_template('guru.html',data=data)
 @app.route('/contactus',methods=['GET','POST'])
 def contactus():
@@ -94,40 +130,77 @@ def contactus():
         name=request.form['name']
         email=request.form['email']
         msg=request.form['msg']
-        cursor=mydb.cursor(buffered=True)
-        cursor.execute('insert into contact(name,email,message) values(%s,%s,%s)',[name,email,msg])
-        mydb.commit()
-        cursor.close()
+        try:
+            mydb=conn.get_connection()
+            cursor = mydb.cursor(buffered=True)
+            cursor.execute('insert into contact(name,email,message) values(%s,%s,%s)',[name,email,msg])
+            mydb.commit()
+            cursor.close()
+        except Exception as e:
+            print(e)
+        finally:
+            if mydb.is_connected():
+                mydb.close()
         return redirect(url_for('home'))
     return render_template('contact.html')
 @app.route('/profile',methods=['GET','POST'])
 def profile():
-    cursor=mydb.cursor(buffered=True)
-    cursor.execute('select first_name,last_name,phone,img,description from register where email=%s',[session.get('user')])
-    count=cursor.fetchone()
+    try:
+        mydb=conn.get_connection()
+        cursor = mydb.cursor(buffered=True)
+        cursor.execute('select first_name,last_name,phone,img,description from register where email=%s',[session.get('user')])
+        count=cursor.fetchone()
+        cursor.close()
+    except Exception as e:
+        print(e)
+    finally:
+            if mydb.is_connected():
+                mydb.close()
     if request.method=='POST':
         fname=request.form['fname']
         lname=request.form['lname']
         phone=request.form['phone']
         des=request.form['editordata']
         print(fname,lname,phone)
-        cursor=mydb.cursor(buffered=True)
-        cursor.execute('update register set first_name=%s,last_name=%s,phone=%s,description=%s where email=%s',[fname,lname,phone,des,session.get('user')])
-        mydb.commit()
-        cursor.close()
+        try:
+            mydb=conn.get_connection()
+            cursor = mydb.cursor(buffered=True)
+            cursor.execute('update register set first_name=%s,last_name=%s,phone=%s,description=%s where email=%s',[fname,lname,phone,des,session.get('user')])
+            mydb.commit()
+            cursor.close()
+        except Exception as e:
+            print(e)
+        finally:
+            if mydb.is_connected():
+                mydb.close()
         return redirect(url_for('profile'))
     return render_template('profile.html',count=count)
 @app.route('/admin')
 def admin():
-    cursor=mydb.cursor(buffered=True)
-    cursor.execute('select * from register')
-    users=cursor.fetchall()
+    try:
+        mydb=conn.get_connection()
+        cursor = mydb.cursor(buffered=True)
+        cursor.execute('select * from register')
+        users=cursor.fetchall()
+        cursor.close()
+    except Exception as e:
+        print(e)
+    finally:
+        if mydb.is_connected():
+            mydb.close()
     return render_template('admindashboard.html',users=users)
 @app.route('/upadte/<name>',methods=['GET','POST'])
 def update(name):
-    cursor=mydb.cursor(buffered=True)
-    cursor.execute('select first_name,last_name,phone,img,description from register where first_name=%s',[name])
-    count=cursor.fetchone()
+    try:
+        mydb=conn.get_connection()
+        cursor = mydb.cursor(buffered=True)
+        cursor.execute('select first_name,last_name,phone,img,description from register where first_name=%s',[name])
+        count=cursor.fetchone()
+    except Exception as e:
+        print(e)
+    finally:
+            if mydb.is_connected():
+                mydb.close()
     print(count)
     if request.method=='POST':
         fname=request.form['fname']
@@ -135,18 +208,32 @@ def update(name):
         phone=request.form['phone']
         des=request.form['editordata']
         print(fname,lname,phone)
-        cursor=mydb.cursor(buffered=True)
-        cursor.execute('update register set first_name=%s,last_name=%s,phone=%s,description=%s where first_name=%s',[fname,lname,phone,des,name])
-        mydb.commit()
-        cursor.close()
+        try:
+            mydb=conn.get_connection()
+            cursor = mydb.cursor(buffered=True)
+            cursor.execute('update register set first_name=%s,last_name=%s,phone=%s,description=%s where first_name=%s',[fname,lname,phone,des,name])
+            mydb.commit()
+            cursor.close()
+        except Exception as e:
+            print(e)
+        finally:
+            if mydb.is_connected():
+                mydb.close()
         return redirect(url_for('update',name=name))
     return render_template('profile.html',count=count)
 @app.route('/delete/<name>')
 def delete(name):
-    cursor=mydb.cursor(buffered=True)
-    cursor.execute('delete from register where first_name=%s',[name])
-    mydb.commit()
-    cursor.close()
+    try:
+        mydb=conn.get_connection()
+        cursor = mydb.cursor(buffered=True)
+        cursor.execute('delete from register where first_name=%s',[name])
+        mydb.commit()
+        cursor.close()
+    except Exception as e:
+        print(e)
+    finally:
+            if mydb.is_connected():
+                mydb.close()
     return redirect(url_for('admin'))
 @app.route('/adminregister',methods=['Get','POST'])
 def adminregister():
@@ -165,9 +252,11 @@ def adminregister():
         img.save(os.path.join(static_path,filename))
 
         try:
-            cursor=mydb.cursor(buffered=True)
+            mydb=conn.get_connection()
+            cursor = mydb.cursor(buffered=True)
             cursor.execute('select count(*) from register where first_name=%s',[fname])
             count=cursor.fetchone()[0]
+            cursor.close()
             print(count)
             if count==1:
                 raise Exception
@@ -175,11 +264,15 @@ def adminregister():
             flash('user already existed')
             return redirect(url_for('admin'))
         else:
-            cursor=mydb.cursor(buffered=True)
+            mydb=conn.get_connection()
+            cursor = mydb.cursor(buffered=True)
             cursor.execute('insert into register(first_name,last_name,phone,email,password,img) values(%s,%s,%s,%s,%s,%s)',[fname,lname,phone,email,password,filename])
             mydb.commit()
             cursor.close()
             flash('Your Details has registered successfully')
             return redirect(url_for('admin'))
+        finally:
+            if mydb.is_connected():
+                mydb.close()
     return redirect(url_for('admin'))
 app.run(debug=True)
